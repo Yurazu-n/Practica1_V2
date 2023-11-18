03ee44a07d815632cb7a6a85372c9af4d07c519b
Owen <yurazunew@gmail.com>
2023-11-18 16:38:30 +0000

    MainTest testing class created

---
+++ src/main/java/org/example/MainTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.example.control.WeatherControl;
+import org.example.view.WeatherInterface;
+
+import java.sql.SQLException;
+
+public class MainTest {
+    public static void main(String[] args) throws SQLException {
+        WeatherInterface weatherInterface = new WeatherInterface();
+        WeatherControl weatherControl = new WeatherControl();
+        weatherControl.execute(args[0], args[1]);
+
+        weatherInterface.run(args[0]);
+    }
+}
